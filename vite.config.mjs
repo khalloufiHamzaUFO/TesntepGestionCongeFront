fcf4fde12b4a1e9d03a13378ab5ad8f3560aed8d
khalloufiHamzaUFO <hamza.khalloufi@esprit.tn>
2024-10-15 23:03:06 +0100

    Test

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,38 +3,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
-// ----------------------------------------------------------------------
-
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
-  base: '/', // accessing env variable is not possible here. So hard coding this.
+  base: '/', // Base URL for the project
   define: {
     global: 'window',
-    'process.env': {} // Prevents "process is not defined" error
+    'process.env': {}
   },
   resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.join(process.cwd(), 'node_modules/$1')
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.join(process.cwd(), 'src/$1')
-      }
-    ]
-  },
-  build: {
-    rollupOptions: {
-      external: ['axios'] // Exclude axios from the bundle
-    },
+    alias: {
+      '~': path.resolve(__dirname, 'node_modules'),
+      'src': path.resolve(__dirname, 'src'),
+    }
   },
   server: {
     open: true,
-    port: 3000
+    port: 3000,
   },
   preview: {
     open: true,
-    port: 3000
-  }
+    port: 3000,
+  },
 });
